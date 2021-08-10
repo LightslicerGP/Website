59dbb4964729ddf7c514725eee6ca7fcb603e701
LightslicerGP <Lightslicerandpuzzlegamer@gmail.com>
2021-08-09 21:26:23 -0400

    Moving everything from the LightslicerGP rep to here
    hopefully this ok

---
+++ Website/About_Me/AboutMe.js
@@ -0,0 +1,41 @@
+const youtubeKey = 'AIzaSyALqIoHfT_TGIAm3cGfblgX2F0PmtqRxuA'
+const youtubeUser = 'UCiGIp50poRZRIAuRt604uRg'
+const subCount = document.getElementById('subcount')
+const viewCount = document.getElementById('viewcount')
+const videoCount = document.getElementById('videocount')
+
+let getSubscribers = () => {
+  fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${youtubeUser}&key=${youtubeKey}`)
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      console.log(data);
+      subCount.innerHTML = data.items[0].statistics.subscriberCount;
+    })
+}
+getSubscribers();
+
+let getViews = () => {
+  fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${youtubeUser}&key=${youtubeKey}`)
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      console.log(data);
+      viewCount.innerHTML = data.items[0].statistics.viewCount;
+    })
+}
+getViews();
+
+let getVideos = () => {
+  fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${youtubeUser}&key=${youtubeKey}`)
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      console.log(data);
+      videoCount.innerHTML = data.items[0].statistics.videoCount;
+    })
+}
+getVideos();
