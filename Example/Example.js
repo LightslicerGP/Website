01292ca4306760b1274fa62016263c5b0a40c1ff
LightslicerGP <Lightslicerandpuzzlegamer@gmail.com>
2021-09-19 20:00:21 -0400

    added loading to example page

--- Example/Example.js
+++ Example/Example.js
@@ -1,14 +1,17 @@
+window.addEventListener("load", function() {
+  const loader = this.document.querySelector(".loader");
+  loader.className += " hidden";
+})
+
 const button = document.getElementById("navbutton");
 const navlinks = document.querySelector("header");
 const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
 const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
 
-button.addEventListener("click", () => 
-  {
-    navlinks.classList.toggle("open");
-    button.classList.toggle("open");
-  }
-);
+button.addEventListener("click", () => {
+  navlinks.classList.toggle("open");
+  button.classList.toggle("open");
+});
 
 function clicked() {
   var dropdown = document.getElementById("dropdown-content");
