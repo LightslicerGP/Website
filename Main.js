20e79e2056be7e2ad7c257af5c03616ccd479398
LightslicerGP <Lightslicerandpuzzlegamer@gmail.com>
2021-09-26 19:50:38 -0400

    Implemented beta features into main webpage

--- Main.js
+++ Main.js
@@ -1,8 +1,37 @@
 const youtubeKey = 'AIzaSyALqIoHfT_TGIAm3cGfblgX2F0PmtqRxuA'
 const youtubeUser = 'UCiGIp50poRZRIAuRt604uRg'
-const subCount = document.getElementById('subcount')
-const viewCount = document.getElementById('viewcount')
-const videoCount = document.getElementById('videocount')
+const subCount = document.getElementById('subcount');
+const viewCount = document.getElementById('viewcount');
+const videoCount = document.getElementById('videocount');
+
+const button = document.getElementById("navbutton");
+const navlinks = document.querySelector("header");
+
+window.addEventListener("load", function() {
+  const loader = document.getElementById("loader");
+  loader.className += " hidden";
+  setTimeout(function() {
+    loader.remove();
+  }, 1500);
+});
+
+button.addEventListener("click", () => {
+  navlinks.classList.toggle("open");
+  button.classList.toggle("open");
+});
+
+function clicked() {
+  var dropdown = document.getElementById("dropdown-content");
+  var dropdownimg = document.getElementById("dropdownimg");
+  if (dropdown.style.display === "none") {
+    dropdown.style.display = "flex";
+    dropdownimg.style.transform = "rotate(90deg)";
+  } else {
+    dropdown.style.display = "none";
+    dropdown.style.height = null;
+    dropdownimg.style.transform = null;
+  }
+};
 
 let getSubscribers = () => {
   fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${youtubeUser}&key=${youtubeKey}`)
@@ -39,7 +68,6 @@ let getVideos = () => {
     })
 }
 getVideos();
-
 
 var modal = document.getElementById("temp");
 window.onclick = function(event) {
