c5a2a8d0d70cf6e5d4e11770dae818bdef7f3760
LightslicerGP <Lightslicerandpuzzlegamer@gmail.com>
2022-03-03 22:22:35 -0500

    cleaned up/removed extra code

--- Main.js
+++ Main.js
@@ -2,9 +2,6 @@
 const youtubeKey = 'AIzaSyALqIoHfT_TGIAm3cGfblgX2F0PmtqRxuA'
 const youtubeUser = 'UCiGIp50poRZRIAuRt604uRg'
 */
-const subCount = document.getElementById('subcount');
-const viewCount = document.getElementById('viewcount');
-const videoCount = document.getElementById('videocount');
 
 let getYTInfo = () => {
   fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCiGIp50poRZRIAuRt604uRg&key=AIzaSyALqIoHfT_TGIAm3cGfblgX2F0PmtqRxuA`)
@@ -13,11 +10,12 @@ let getYTInfo = () => {
     })
     .then(data => {
       console.log(data);
-      subCount.innerHTML = data.items[0].statistics.subscriberCount;
-      viewCount.innerHTML = data.items[0].statistics.viewCount;
-      videoCount.innerHTML = data.items[0].statistics.videoCount;
+      document.getElementById('subcount').innerHTML = data.items[0].statistics.subscriberCount;
+      document.getElementById('viewcount').innerHTML = data.items[0].statistics.viewCount;
+      document.getElementById('videocount').innerHTML = data.items[0].statistics.videoCount;
     })
 }
+
 getYTInfo();
 
 var modal = document.getElementById("temp");
